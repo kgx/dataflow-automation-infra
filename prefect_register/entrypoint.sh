66575d39c172ae8ef26cfa29b95fea4019e17484
Maikel Penz <maikelpenz@gmail.com>
2020-12-09 20:02:58 +1300

    workflow

--- prefect_register/entrypoint.sh
+++ prefect_register/entrypoint.sh
@@ -7,4 +7,7 @@ echo "list files"
 ls
 echo "------"
 
-git
+git clone --depth 1 --branch feature/add_workflow --filter=blob:none --no-checkout https://github.com/maikelpenz/dataflow-sample-workflow.git
+cd dataflow-sample-workflow
+git checkout feature/add_workflow -- workflow
+ls
