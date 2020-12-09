58c768d87f82b7605738340397ef4c9768cb47b2
Maikel Penz <maikelpenz@gmail.com>
2020-12-09 20:47:01 +1300

    workflow

--- prefect_register/entrypoint.sh
+++ prefect_register/entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+git_url=$1
+repository_name=${$(basename $git_url)%.*}
+branch_name=$2
+commit_sha=$3
+
+#echo "Hello $1"
 #time=$(date)
 #echo "::set-output name=time::$time"
-echo "list files"
-ls
-echo "------"
 
-git clone --branch feature/add_workflow --no-checkout https://github.com/maikelpenz/dataflow-sample-workflow.git
-cd dataflow-sample-workflow
-git checkout e678cd06ba5db1492b0297e5cd387aab029d3eaf -- workflow
+git clone --branch $branch_name \
+         --no-checkout $git_url
+cd $repository_name
+git checkout $commit_sha -- workflow
 ls
-ls workflow
+
+# git clone --branch feature/add_workflow --no-checkout https://github.com/maikelpenz/dataflow-sample-workflow.git
+# cd dataflow-sample-workflow
+# git checkout e678cd06ba5db1492b0297e5cd387aab029d3eaf -- workflow
