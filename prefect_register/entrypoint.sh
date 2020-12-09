4f7eab51930a9f2789ba93e6b46bfbb1d9272cd7
Maikel Penz <maikelpenz@gmail.com>
2020-12-09 21:05:31 +1300

    workflow change

--- prefect_register/entrypoint.sh
+++ prefect_register/entrypoint.sh
@@ -1,22 +1,19 @@
 #!/bin/sh -l
 
+# set variables
 git_url=$1
 branch_name=$2
 commit_sha=$3
-
+workflow_path=$4
 git_url_basename=$(basename $git_url)
 repository_name=${git_url_basename%.*}
 
-#echo "Hello $1"
-#time=$(date)
-#echo "::set-output name=time::$time"
-
+# clone workflow into container
 git clone --branch $branch_name \
          --no-checkout $git_url
 cd $repository_name
-git checkout $commit_sha -- workflow
-ls
+git checkout $commit_sha -- $workflow_path
 
-# git clone --branch feature/add_workflow --no-checkout https://github.com/maikelpenz/dataflow-sample-workflow.git
-# cd dataflow-sample-workflow
-# git checkout e678cd06ba5db1492b0297e5cd387aab029d3eaf -- workflow
+ls 
+echo "subfolder"
+ls $workflow_path
