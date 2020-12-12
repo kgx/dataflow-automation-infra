413460dff407b278a3ad74173d793a5f669b17b5
Maikel Penz <maikelpenz@gmail.com>
2020-12-13 10:22:21 +1300

    workflow

--- prefect_register/entrypoint.sh
+++ prefect_register/entrypoint.sh
@@ -8,12 +8,17 @@ workflow_path=$4
 git_url_basename=$(basename $git_url)
 repository_name=${git_url_basename%.*}
 
+echo $git_url
+echo $branch_name
+echo $commit_sha
+echo $workflow_path
+echo $git_url_basename
+echo $repository_name
+
 # clone workflow into container
 git clone --branch $branch_name \
          --no-checkout $git_url
 cd $repository_name
-
-echo $workflow_path
 
 git checkout $commit_sha --$workflow_path
 
