0ddea3c5ab7ee6e28082a5db517ccce58f72482f
Maikel Penz <maikelpenz@gmail.com>
2020-12-14 21:13:40 +1300

    workflow

--- prefect_register/entrypoint.sh
+++ prefect_register/entrypoint.sh
@@ -8,20 +8,16 @@ workflow_path=$4
 git_url_basename=$(basename $git_url)
 repository_name=${git_url_basename%.*}
 
-echo "ls tmp1"
-ls /tmp/
-
-echo "pwd"
-pwd
-
 # clone workflow into container
 git clone --branch $branch_name \
          --no-checkout $git_url
-echo "ls"
-ls         
+
 cd $repository_name
 git checkout $commit_sha -- $workflow_path
-mv $workflow_path /tmp/$workflow_path
 
-echo "ls tmp"
-ls /tmp/
+# move to /tmp/
+mv $workflow_path /tmp/$workflow_path
+# move flow register into the flow folder
+mv /tmp/workflow_register.py /tmp/$workflow_path/workflow_register.py
+echo "ls"
+ls /tmp/$workflow_path
