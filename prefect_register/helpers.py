4af1899227e3c99f7360191eecdca604cc4db1ff
Maikel Penz <maikelpenz@gmail.com>
2020-12-16 19:38:40 +1300

    workflow

---
+++ prefect_register/helpers.py
@@ -0,0 +1,62 @@
+import base64
+import json
+import subprocess
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def ecr_authenticate():
+    aws_session = boto3.Session()
+    ecr_client = aws_session.client("ecr")
+
+    ecr_credentials = ecr_client.get_authorization_token()["authorizationData"][0]
+
+    ecr_username = "AWS"
+
+    ecr_password = (
+        base64.b64decode(ecr_credentials["authorizationToken"])
+        .replace(b"AWS:", b"")
+        .decode("utf-8")
+    )
+
+    ecr_url = ecr_credentials["proxyEndpoint"]
+
+    subprocess.run(["docker", "login", "-u", ecr_username, "-p", ecr_password, ecr_url])
+
+
+def get_prefect_token(secret_name: str):
+    client = boto3.client(service_name="secretsmanager", region_name="ap-southeast-2")
+    secret = None
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "DecryptionFailureException":
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "InternalServiceErrorException":
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "InvalidParameterException":
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "InvalidRequestException":
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "ResourceNotFoundException":
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if "SecretString" in get_secret_value_response:
+            secret = get_secret_value_response["SecretString"]
+        else:
+            secret = base64.b64decode(get_secret_value_response["SecretBinary"])
+
+    return json.loads(secret).get(secret_name)
--- prefect_register/workflow_register.py
+++ prefect_register/workflow_register.py
@@ -2,5 +2,61 @@ from prefect import Client
 from prefect.environments import FargateTaskEnvironment
 from prefect.environments.storage import Docker
 
+from .helpers import ecr_authenticate, get_prefect_token
+
 flow_module = __import__("flow")
-print("dalhe")
+
+flow_module.environment = FargateTaskEnvironment(
+    requiresCompatibilities=["FARGATE"],
+    region="ap-southeast-2",
+    labels=["dev_dataflow_automation"],
+    taskDefinition="dev_sample_workflow",
+    family="dev_sample_workflow",
+    cpu="512",
+    memory="3072",
+    networkMode="awsvpc",
+    networkConfiguration={
+        "awsvpcConfiguration": {
+            "assignPublicIp": "ENABLED",
+            "subnets": [
+                "subnet-05c3fcfce9275d195",
+                "subnet-02410b882477eea13",
+                "subnet-05d07984082846a2b",
+            ],
+            "securityGroups": [],
+        }
+    },
+    containerDefinitions=[
+        {
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-region": "ap-southeast-2",
+                    "awslogs-group": "dev_dataflow_automation_workflows",
+                    "awslogs-stream-prefix": "dev_sample_workflow",
+                },
+            }
+        }
+    ],
+    executionRoleArn="arn:aws:iam::844814218183:role/dev_prefect_workflow_ecs_task_execution_role",
+    taskRoleArn="arn:aws:iam::844814218183:role/dev_prefect_workflow_ecs_task_role",
+    cluster="dev_dataflow_automation_workflows",
+)
+
+# Set the flow storage. Where to get the code from
+flow_module.storage = Docker(
+    registry_url="844814218183.dkr.ecr.ap-southeast-2.amazonaws.com",
+    image_name="dev_sample_workflow",
+    image_tag="latest",
+    python_dependencies=["boto3"],
+    env_vars={"PYTHONPATH": "/opt/prefect/flows"},
+)
+
+# Authenticate to ECR as the registration process pushes the image to AWS
+ecr_authenticate()
+
+# Instantiate the prefect client
+prefect_client = Client(api_token=get_prefect_token(secret_name="prefectregistertoken"))
+
+# Register the Workflow
+prefect_client.register(flow=flow_module, project_name="dev_dataflow_automation")
